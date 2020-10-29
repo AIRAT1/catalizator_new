290c5843b76b35f3c6e4ba0e078c3cc2f0408eed
Ayrat <ayrat1@mail.ru>
2020-10-29 08:49:04 +0300

    init project

---
+++ src/main/java/de/catalizator_new/CatalizatorNewApplication.java
@@ -0,0 +1,13 @@
+package de.catalizator_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalizatorNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalizatorNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/catalizator_new/CatalizatorNewApplicationTests.java
@@ -0,0 +1,13 @@
+package de.catalizator_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalizatorNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
